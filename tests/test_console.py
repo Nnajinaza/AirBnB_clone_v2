0f9a0627a4cdfc4fef0bf56de20a9d0515583484
PC-Ngumoha <pcngumoha@gmail.com>
2022-10-11 19:23:40 +0000

    Made changes

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+'''Tests the functionality of the hbnb console.
+'''
+
+import unittest
+from unittest.mock import Mock
+
+mock = Mock()
+
+
+class TestConsole(unittest.TestCase):
+    '''Tests the functionality of the console
+    '''
+
+
+if __name__ == '__main__':
+    unittest.main()
