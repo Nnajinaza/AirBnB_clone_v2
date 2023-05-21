06803932754cb9fc80824b99075eb4d0fdc88637
Nnajinaza <nnajinaza@gmail.com>
2023-05-21 17:58:43 +0000

    update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+""" Renders a Web page with the list of all the States stores on the DB
+"""
 from flask import Flask, render_template
-import models
+from models import storage
+from models.state import State
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def refresh(exception):
-        models.storage.close()
+def teardown_session(exception):
+    """ Closes the current session with the DB """
+    storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def display_states_list():
+    """ Renders the list of states in a web page """
+    states_list = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states_list)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
-
-
-if __name__ == "__main__":
-        app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
